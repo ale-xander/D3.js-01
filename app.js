7412951a56d42adea8ad80a0fdd9cdfd81056abd
alexiliev <iliev.alex@outlook.com>
2021-02-04 23:50:50 -0600

    groups

--- app.js
+++ app.js
@@ -8,26 +8,33 @@ const svg = canvas.append("svg")
     .attr("height", 600)
     .attr("width", 600)// add an attribute
 
-
-
+const group = svg.append('g')
+    .attr("transform", "translate(0,100)")
 
 //append shapes to svg container
-svg.append("rect")
-    .attr("height", 200)
-    .attr("width", 100)
+group.append("rect")
+    .attr("height", 100)
+    .attr("width", 200)
     .attr("fill", "#0339fc")
     .attr("x", 20)
     .attr("y", 20)
 
-svg.append("circle")
+group.append("circle")
     .attr("r", 50)
     .attr("cx", 300)
     .attr("cy", 70)
     .attr("fill", "#7b03fc")
 
-svg.append("line")
+group.append("line")
     .attr("x1", 370)
     .attr("x2", 400)
     .attr("y1", 20)
     .attr("y2", 320)
     .attr("stroke", "#d13b04")
+
+svg.append("text")
+    .attr("x", 20)
+    .attr("y", 200)
+    .attr("fill", "goldenrod")
+    .text("what's up")
+    .style("font-family", "helvetica")
