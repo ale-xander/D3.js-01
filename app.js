95800c6c7e710b34e83cc88e89c41236b62cecb8
alexiliev <iliev.alex@outlook.com>
2021-02-04 23:22:11 -0600

    adding d3 lib

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("hahahah")
+
+//refereance to canvas element
+const canvas = d3.select(".canvas")
+
+// create svg container
+const svg = canvas.append("svg")
+
+//append shapes to svg container
+svg.append("rect")
+svg.append("circ")
+svg.append("line")
